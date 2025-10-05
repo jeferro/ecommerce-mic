b99cd22794646acc20ffe0322fe55d2dd22b518c
Juan M. Estevez Ferro <jeferro@gmail.com>
2025-10-05 10:01:58 +0200

    Add classes AuditedDocument and AuditorAwareImpl

--- code/lib-shared/src/main/java/com/jeferro/shared/auth/infrastructure/mongo/services/MongoAuditorAware.java
+++ code/lib-shared/src/main/java/com/jeferro/shared/auth/infrastructure/mongo/services/MongoAuditorAware.java
@@ -1,30 +1,16 @@
 package com.jeferro.shared.auth.infrastructure.mongo.services;
 
 import java.util.Optional;
+
+import com.jeferro.shared.auth.infrastructure.ContextManager;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MongoAuditorAware implements AuditorAware<String> {
 
-  private static final String UNKNOWN = "unknown";
-
-  @Override
-  public Optional<String> getCurrentAuditor() {
-    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-    var value = UNKNOWN;
-
-    if (authentication != null) {
-      final Object principal = authentication.getPrincipal();
-
-      if (principal instanceof String) {
-        value = (String) principal;
-      }
-    }
-
-    return Optional.of(value);
-  }
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		return Optional.of(ContextManager.getAuth().getUsername());
+	}
 }
