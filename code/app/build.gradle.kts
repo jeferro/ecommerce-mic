d2aa52fc91e3fe9f97ab75c110c2719d8ae0815d
Juan M. Estévez Ferro <jeferro@gmail.com>
2025-12-03 09:39:51 +0100

    Create folder tools

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -48,17 +48,17 @@ apiFirstGenerator {
         ApiFirstGeneratorSpec().apply {
             name = "users-v1"
             basePackage = "com.jeferro.ecommerce.users.users.infrastructure.rest_api"
-            specFile = file("${projectDir}/../../apis/rest/users/users.v1.yml")
+            specFile = file("${projectDir}/../../apis/rest/v1/users/users.v1.yml")
         },
         ApiFirstGeneratorSpec().apply {
             name = "products-v1"
             basePackage = "com.jeferro.ecommerce.products.product_versions.infrastructure.rest_api"
-            specFile = file("${projectDir}/../../apis/rest/product_versions/product_versions.v1.yml")
+            specFile = file("${projectDir}/../../apis/rest/v1/product_versions/product_versions.v1.yml")
         },
         ApiFirstGeneratorSpec().apply {
             name = "reviews-v1"
             basePackage = "com.jeferro.ecommerce.products.reviews.infrastructure.rest_api"
-            specFile = file("${projectDir}/../../apis/rest/reviews/reviews.v1.yml")
+            specFile = file("${projectDir}/../../apis/rest/v1/reviews/reviews.v1.yml")
         }
     )
 }
